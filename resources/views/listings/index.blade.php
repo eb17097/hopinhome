cbc90cf82d2ed5721d329285ead1de521c3f078b
Emils <emils.boreiko@gmail.com>
2026-02-08 22:25:53 +0200

    listings

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -230,8 +230,19 @@
                 </button>
             </div>
 
-            <div>
+            <div class="pt-4 border-b border-gray-100 pb-8">
                 <h4 class="font-medium text-gray-900 mb-4 text-lg">Recommended for you</h4>
+                <ul class="space-y-3">
+                    <li><a href="#" class="text-sm text-gray-500 hover:text-blue-600 transition">2 bedroom apartments for rent in Dubai</a></li>
+                    <li><a href="#" class="text-sm text-gray-500 hover:text-blue-600 transition">3 bedroom houses for rent in the UAE</a></li>
+                    <li><a href="#" class="text-sm text-gray-500 hover:text-blue-600 transition">Villas for rent in Dubai</a></li>
+                    <li><a href="#" class="text-sm text-gray-500 hover:text-blue-600 transition">2 bedroom apartments in Abu Dhabi</a></li>
+                    <li><a href="#" class="text-sm text-gray-500 hover:text-blue-600 transition">Penthouses for rent in Downtown Dubai</a></li>
+                </ul>
+            </div>
+
+            <div>
+                <h4 class="font-medium text-gray-900 mb-4 text-lg">Popular searches</h4>
                 <ul class="space-y-3">
                     <li><a href="#" class="text-sm text-gray-500 hover:text-blue-600 transition">2 bedroom apartments for rent in Dubai</a></li>
                     <li><a href="#" class="text-sm text-gray-500 hover:text-blue-600 transition">3 bedroom houses for rent in the UAE</a></li>
