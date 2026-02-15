137ad6d192fb566d94b3899d476cbf391e20ea9a
Emils <emils.boreiko@gmail.com>
2026-02-15 15:52:09 +0200

    manager

--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -1,7 +1,7 @@
 <x-manager-layout>
     <div class="bg-white">
         <x-header />
-        <div class="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8 pt-28">
             <div class="flex">
                 <div class="w-1/4">
                     <x-manager.sidebar />
