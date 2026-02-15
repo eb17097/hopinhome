f6cedf6f5b4319306c52b0f766b3612868c0d927
Emils <emils.boreiko@gmail.com>
2026-02-15 22:38:59 +0200

    listing video

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -50,7 +50,7 @@ class ListingController extends Controller
                 'price' => 'required|numeric',
                 'duration' => 'required|integer',
                 'renewal_type' => 'required|string',
-                'video_file' => 'nullable|file|mimetypes:mp4,mov,avi,wmv|max:51200', // 50MB Max
+                'video_file' => 'nullable|file|mimes:mp4,mov,avi,wmv|max:51200', // 50MB Max
                 'photos' => 'nullable|array',
                 'photos.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
                 'features' => 'nullable|array',
