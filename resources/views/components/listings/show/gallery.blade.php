1554b43fee07236553e84aa691cc15c3d973a14a
Emils <emils.boreiko@gmail.com>
2026-02-17 13:19:38 +0200

    videoplay[D[D[D

--- resources/views/components/listings/show/gallery.blade.php
+++ resources/views/components/listings/show/gallery.blade.php
@@ -4,7 +4,13 @@
     use Illuminate\Support\Str;
 @endphp
 
-<div x-data="{ isVideoModalOpen: false }" class="mt-6 relative">
+<div x-data="{ isVideoModalOpen: false }" @keydown.escape.window="isVideoModalOpen = false" x-init="$watch('isVideoModalOpen', value => { 
+    if (value) {
+        $nextTick(() => $refs.videoPlayer.play());
+    } else {
+        $refs.videoPlayer.pause();
+    }
+})" class="mt-6 relative">
     <div class="grid grid-cols-2 gap-[16px]">
         {{-- Main Image --}}
         <div class="col-span-1">
@@ -56,7 +62,7 @@
     @if($listing->video_url)
     <div x-show="isVideoModalOpen" style="display: none;" class="fixed z-50 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div class="flex items-center justify-center min-h-screen p-4 text-center">
-            <div x-show="isVideoModalOpen" @click.away="isVideoModalOpen = false"
+            <div x-show="isVideoModalOpen"
                  x-transition:enter="ease-out duration-300"
                  x-transition:enter-start="opacity-0"
                  x-transition:enter-end="opacity-100"
@@ -66,16 +72,18 @@
                  class="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
             <div x-show="isVideoModalOpen"
+                 @click.away="isVideoModalOpen = false"
                  x-transition:enter="ease-out duration-300"
                  x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                  x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
                  x-transition:leave="ease-in duration-200"
                  x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                  x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                 class="inline-block bg-black rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 w-full max-w-4xl">
+                 class="inline-block bg-black rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 w-full max-w-4xl"
+                 @click.stop>
                 
                 <div class="relative">
-                    <video class="w-full h-auto" src="{{ Str::startsWith($listing->video_url, 'http') ? $listing->video_url : Storage::url($listing->video_url) }}" controls autoplay></video>
+                    <video x-ref="videoPlayer" class="w-full h-auto" src="{{ Str::startsWith($listing->video_url, 'http') ? $listing->video_url : Storage::url($listing->video_url) }}" controls></video>
                     <button @click="isVideoModalOpen = false" class="absolute top-4 right-4 text-white">
                         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
                     </button>
