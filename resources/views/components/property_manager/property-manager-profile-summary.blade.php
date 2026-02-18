7b6c09386cc9b7e927b8ce36ca53c0443ac91013
Emils <emils.boreiko@gmail.com>
2026-02-18 16:53:56 +0200

    pm

--- resources/views/components/property_manager/property-manager-profile-summary.blade.php
+++ resources/views/components/property_manager/property-manager-profile-summary.blade.php
@@ -1,29 +1,19 @@
 @php use Illuminate\Support\Facades\Auth; @endphp
 
-<div class="bg-white border border-light-gray rounded-lg shadow-sm p-6">
-    <div class="flex items-start space-x-4">
-        <div class="w-16 h-16 rounded-full border border-light-gray overflow-hidden">
+<div class="bg-[#f9f9f8] rounded-[6px] p-4 flex items-center justify-between">
+    <div class="flex items-center space-x-4">
+        <div class="w-16 h-16 rounded-full border border-light-gray overflow-hidden shrink-0">
             <img alt="profile picture" class="h-full w-full object-cover" src="{{ Auth::user()->profile_photo_url ?? asset('images/profile_picture.png') }}">
         </div>
-        <div class="ml-4">
-            <div class="flex items-center gap-2">
-                <h3 class="text-lg font-medium text-black">{{ Auth::user()->name }}</h3>
-                <img alt="verified user" class="h-5 w-5" src="{{ asset('images/verified_user.svg') }}">
+        <div>
+            <div class="flex items-center gap-1">
+                <h3 class="text-[18px] font-medium text-[#1e1d1d] leading-[1.28]">{{ Auth::user()->name }}</h3>
+                <img alt="verified user" class="w-[18px] h-[18px]" src="{{ asset('images/verified_user.svg') }}">
             </div>
-            <p class="text-sm font-medium text-gray-600">
+            <p class="text-[14px] font-medium text-[#1e1d1d] leading-[1.5]">
                 {{ Auth::user()->email }} • verified
             </p>
         </div>
     </div>
-    <hr class="my-4 border-light-gray">
-    <div class="grid grid-cols-1 gap-6">
-        <a href="{{ route('profile.edit') }}" class="bg-white border border-light-gray rounded-lg p-6 flex justify-between items-center">
-            <span class="text-base font-medium text-black">Edit bio</span>
-            <img alt="arrow forward" class="h-5 w-5" src="{{ asset('images/arrow_forward_black.svg') }}">
-        </a>
-        <a href="#" class="bg-white border border-light-gray rounded-lg p-6 flex justify-between items-center">
-            <span class="text-base font-medium text-black">Change profile picture</span>
-            <img alt="arrow forward" class="h-5 w-5" src="{{ asset('images/arrow_forward_black.svg') }}">
-        </a>
-    </div>
+    <img alt="arrow forward" class="w-[18px] h-[18px] brightness-0 opacity-70" src="{{ asset('images/arrow_forward.svg') }}">
 </div>
