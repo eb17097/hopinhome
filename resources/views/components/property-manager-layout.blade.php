653338c07b7b0126b62401e0f51dd379260424f0
Emils <emils.boreiko@gmail.com>
2026-02-18 17:26:13 +0200

    pm

--- resources/views/components/property-manager-layout.blade.php
+++ resources/views/components/property-manager-layout.blade.php
@@ -23,13 +23,21 @@
         <div x-data="{ sidebarOpen: true }" @toggle-sidebar.window="sidebarOpen = !sidebarOpen" class="min-h-screen bg-white">
 
 
-            <div class="flex">
-                <div x-show="sidebarOpen" id="property-manager-sidebar" class="relative w-[232px] h-screen bg-white border-r border-light-gray shadow-[0px_0px_64px_0px_rgba(0,0,0,0.03)] pb-8 overflow-y-auto transition-all duration-300 ease-in-out">
+            <div class="flex overflow-hidden">
+                <div x-show="sidebarOpen" 
+                     x-transition:enter="transition ease-out duration-300"
+                     x-transition:enter-start="-translate-x-full"
+                     x-transition:enter-end="translate-x-0"
+                     x-transition:leave="transition ease-in duration-300"
+                     x-transition:leave-start="translate-x-0"
+                     x-transition:leave-end="-translate-x-full"
+                     id="property-manager-sidebar" 
+                     class="relative w-[232px] h-screen bg-white border-r border-light-gray shadow-[0px_0px_64px_0px_rgba(0,0,0,0.03)] pb-8 overflow-y-auto shrink-0">
                     <div class="flex justify-between items-center px-4 pt-[27px] pb-4">
                         <div class="flex items-center gap-2">
                             <img src="{{ asset('images/hopinhome_logo_blue.svg') }}" alt="HopInHome Logo" class="h-6 w-auto">
                         </div>
-                        <button @click="sidebarOpen = false" class="text-gray-400 hover:text-gray-600">
+                        <button @click="sidebarOpen = false" class="text-gray-400 hover:text-gray-600 transition-colors">
                             <img src="{{ asset('images/left_panel_close.svg') }}" alt="Close" class="h-6 w-6">
                         </button>
                     </div>
@@ -37,9 +45,9 @@
                         <x-property_manager.property-manager-sidebar />
                     </div>
                 </div>
-                <main class="flex-1 transition-all duration-300 ease-in-out">
+                <main class="flex-1 h-screen overflow-y-auto transition-all duration-300 ease-in-out bg-white">
                     <x-property_manager.property-manager-header />
-                    <div class="px-8">
+                    <div class="px-8 py-6">
                         {{ $slot }}
                     </div>
                 </main>
--- resources/views/components/property_manager/property-manager-header.blade.php
+++ resources/views/components/property_manager/property-manager-header.blade.php
@@ -1,15 +1,15 @@
 @php use Illuminate\Support\Facades\Auth; @endphp
-<nav class="bg-white border-b border-light-gray h-16 flex items-center px-8 justify-between">
+<nav class="bg-white border-b border-light-gray h-16 flex items-center px-6 justify-between">
     <div class="flex items-center space-x-4">
-        {{-- Placeholder for search or other header elements --}}
+        <h2 class="text-[15px] font-medium text-[#1e1d1d]">Dashboard</h2>
     </div>
 
     <div class="flex items-center space-x-6">
-        <a href="{{ route('home') }}" class="text-[14px] font-medium text-[#464646] hover:text-black transition-colors">Go to homepage</a>
+        <a href="{{ route('home') }}" class="text-[15px] font-medium text-[#1e1d1d] hover:text-black transition-colors">Go to homepage</a>
 
         <div class="relative cursor-pointer">
-            <img src="{{ asset('images/notifications.svg') }}" alt="Notifications" class="w-6 h-6 opacity-70">
-            <div class="absolute -top-1 -right-1 flex items-center justify-center w-[18px] h-[18px] rounded-full text-white text-[10px] font-medium bg-electric-blue border-2 border-white">
+            <img src="{{ asset('images/notifications.svg') }}" alt="Notifications" class="w-5 h-5 opacity-70">
+            <div class="absolute -top-2 -right-2 flex items-center justify-center w-[18px] h-[18px] rounded-full text-white text-[10px] font-medium bg-electric-blue border-2 border-white">
                 <span>5</span>
             </div>
         </div>
@@ -17,5 +17,9 @@
         <a href="#" class="w-8 h-8 rounded-full border border-light-gray overflow-hidden">
             <img alt="profile picture" class="h-full w-full object-cover" src="{{ Auth::user()->profile_photo_url ?? asset('images/profile_picture.png') }}">
         </a>
+
+        <button @click="$dispatch('toggle-sidebar')" class="bg-light-gray rounded-full p-2 hover:bg-gray-200 transition-colors">
+            <img src="{{ asset('images/dehaze.svg') }}" alt="Menu" class="w-6 h-6">
+        </button>
     </div>
 </nav>
