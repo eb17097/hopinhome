4bbefb999ca76eb095ca88d6e478da257168fd86
Emils <emils.boreiko@gmail.com>
2026-02-18 17:23:20 +0200

    pm

--- resources/views/components/property_manager/property-manager-sidebar.blade.php
+++ resources/views/components/property_manager/property-manager-sidebar.blade.php
@@ -54,6 +54,15 @@
                 <img alt="language" class="w-[18px] h-[18px]" src="{{ asset('images/language_sidebar.svg') }}">
                 <span class="font-medium text-[14px] leading-[1.3]">Regional settings</span>
             </a>
+            <form method="POST" action="{{ route('logout') }}" class="w-full">
+                @csrf
+                <button type="submit" class="flex items-center space-x-[10px] w-full p-2 rounded-[4px] hover:bg-red-50 text-red-600 transition-colors text-left">
+                    <div class="w-[18px] h-[18px] flex items-center justify-center">
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+                    </div>
+                    <span class="font-medium text-[14px] leading-[1.3]">Sign out</span>
+                </button>
+            </form>
         </div>
     </div>
 
