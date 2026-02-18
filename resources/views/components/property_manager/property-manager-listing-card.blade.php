39dbb0f9d178a1b75b91a1f8ab2701268befc1fc
Emils <emils.boreiko@gmail.com>
2026-02-18 17:05:07 +0200

    pm

--- resources/views/components/property_manager/property-manager-listing-card.blade.php
+++ resources/views/components/property_manager/property-manager-listing-card.blade.php
@@ -44,7 +44,7 @@
                 <a href="{{ route('listings.show', $listing->id) }}" class="text-sm font-medium text-electric-blue hover:underline">View</a>
                 <a href="#" class="text-sm font-medium text-gray-700 hover:underline">Edit</a>
                 
-                <form x-on:submit.prevent="showDeleteModal = true" method="POST" action="{{ route('listings.destroy', $listing->id) }}">
+                <form x-on:submit.prevent="showDeleteModal = true" method="POST" action="{{ route('property_manager.listings.destroy', $listing->id) }}">
                     @csrf
                     @method('DELETE')
                     <button type="submit" class="text-sm font-medium text-red-600 hover:underline">Delete</button>
@@ -107,11 +107,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <form method="POST" action="{{ route('listings.destroy', $listing->id) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm">
+                                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                    <form method="POST" action="{{ route('property_manager.listings.destroy', $listing->id) }}">
+                                        @csrf
+                                        @method('DELETE')                        <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm">
                             Delete
                         </button>
                     </form>
