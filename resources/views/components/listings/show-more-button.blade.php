3e9d0fb5201cf11bddafb2f0af85c26967c990bc
Emils <emils.boreiko@gmail.com>
2026-02-21 15:43:26 +0200

    listings

--- resources/views/components/listings/show-more-button.blade.php
+++ resources/views/components/listings/show-more-button.blade.php
@@ -1,6 +1,6 @@
 <div class="pt-8 text-center">
     <button class="inline-flex items-center justify-center gap-2 px-8 py-4 bg-blue-600 text-white rounded-full font-medium text-base hover:bg-blue-700 transition shadow-sm hover:shadow-md">
-        <img src="{{ asset('images/arrow_downward.svg') }}" alt="Show More Arrow" class="w-5 h-5">
+        <img src="{{ asset('images/arrow_downward_white.svg') }}" alt="Show More Arrow" class="w-5 h-5">
         Show more properties
     </button>
 </div>
