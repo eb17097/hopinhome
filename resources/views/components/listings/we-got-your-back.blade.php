1afd5c0e734f8182dd476f9a366250f61cc756c1
Emils <emils.boreiko@gmail.com>
2026-02-21 16:02:27 +0200

    listings

--- resources/views/components/listings/we-got-your-back.blade.php
+++ resources/views/components/listings/we-got-your-back.blade.php
@@ -1,24 +1,24 @@
-<div class="bg-navy-blue rounded-2xl p-10 text-white relative overflow-hidden flex flex-col justify-between h-full min-h-[340px]">
+<div class="bg-navy-blue rounded-[8px] p-8 text-white relative overflow-hidden flex flex-col justify-between min-h-[263px]">
     <!-- Background Graphic (Logo Mark) -->
-    <div class="absolute -bottom-16 -right-12 w-64 h-64 opacity-10 pointer-events-none">
-        <svg viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full h-full rotate-[-15deg]">
-            <path d="M17.489 22.2789L17.9823 27.2133C16.6858 27.615 15.3082 27.8312 13.8801 27.8312C12.452 27.8312 11.0744 27.615 9.77783 27.2133L10.2713 22.2789H17.489Z" fill="currentColor"/>
-            <path d="M5.00179 16.6149L4.43089 22.2792H2.7855C1.0367 19.9509 0 17.0548 0 13.9158C0 8.978 2.56522 4.64096 6.43244 2.17095L5.00179 16.6149Z" fill="currentColor"/>
-            <path d="M10.271 22.2791H6.38379L6.93492 16.8107L6.93497 16.8099L8.49231 1.08723C9.74063 0.559643 11.082 0.209839 12.4849 0.0694805L12.4919 3.8147e-05L10.271 22.2791Z" fill="currentColor"/>
-            <path d="M22.7582 16.6149L23.3291 22.2792H24.9745C26.7234 19.9509 27.7599 17.0548 27.7599 13.9158C27.7599 8.978 25.1947 4.64096 21.3276 2.17095L22.7582 16.6149Z" fill="currentColor"/>
-            <path d="M17.4889 22.2791H21.3762L20.825 16.8107L20.8249 16.8099L19.2676 1.08723C18.0192 0.559643 16.6779 0.209839 15.275 0.0694805L15.2681 3.8147e-05L17.4889 22.2791Z" fill="currentColor"/>
+    <div class="absolute -bottom-24 -right-16 w-80 h-80 opacity-[0.07] pointer-events-none">
+        <svg viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full h-full rotate-[-10deg]">
+            <path d="M17.489 22.2789L17.9823 27.2133C16.6858 27.615 15.3082 27.8312 13.8801 27.8312C12.452 27.8312 11.0744 27.615 9.77783 27.2133L10.2713 22.2789H17.489Z" fill="white"/>
+            <path d="M5.00179 16.6149L4.43089 22.2792H2.7855C1.0367 19.9509 0 17.0548 0 13.9158C0 8.978 2.56522 4.64096 6.43244 2.17095L5.00179 16.6149Z" fill="white"/>
+            <path d="M10.271 22.2791H6.38379L6.93492 16.8107L6.93497 16.8099L8.49231 1.08723C9.74063 0.559643 11.082 0.209839 12.4849 0.0694805L12.4919 3.8147e-05L10.271 22.2791Z" fill="white"/>
+            <path d="M22.7582 16.6149L23.3291 22.2792H24.9745C26.7234 19.9509 27.7599 17.0548 27.7599 13.9158C27.7599 8.978 25.1947 4.64096 21.3276 2.17095L22.7582 16.6149Z" fill="white"/>
+            <path d="M17.4889 22.2791H21.3762L20.825 16.8107L20.8249 16.8099L19.2676 1.08723C18.0192 0.559643 16.6779 0.209839 15.275 0.0694805L15.2681 3.8147e-05L17.4889 22.2791Z" fill="white"/>
         </svg>
     </div>
 
     <div class="relative z-10">
-        <h3 class="text-[40px] font-semibold leading-[1.1] mb-5">We've got your back</h3>
-        <p class="text-white/80 text-[18px] leading-[1.5] max-w-[300px]">
+        <h3 style="font-weight: 500;" class="text-[32px] leading-tight mb-4">We’ve got your back</h3>
+        <p class="text-white/80 text-[16px] leading-relaxed">
             From honest listings to practical insights, HopInHome helps you make rental decisions with clarity and peace of mind.
         </p>
     </div>
 
     <div class="relative z-10">
-        <button class="border border-white rounded-full px-10 py-3 text-[18px] font-medium hover:bg-white hover:text-navy-blue transition-all duration-300">
+        <button class="border border-white rounded-full px-8 py-2.5 text-[16px] font-medium hover:bg-white hover:text-navy-blue transition-colors duration-200">
             Learn more
         </button>
     </div>
