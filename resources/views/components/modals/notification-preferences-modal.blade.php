0df0773495a1054355c989db5c77a17adba80ef4
Emils <emils.boreiko@gmail.com>
2026-02-23 00:58:11 +0200

    notifications

--- resources/views/components/modals/notification-preferences-modal.blade.php
+++ resources/views/components/modals/notification-preferences-modal.blade.php
@@ -171,10 +171,12 @@
                 <div class="mt-10">
                     <button @click="saveSettings()" 
                             :disabled="loading"
-                            class="w-full h-[52px] bg-[#1447d4] hover:bg-[#04247b] text-white font-medium rounded-[8px] transition-all text-[16px] flex items-center justify-center disabled:opacity-50">
+                            class="w-full h-[52px] bg-[#1447d4] hover:bg-[#04247b] text-white font-medium rounded-[8px] transition-all text-[16px] flex items-center justify-center disabled:opacity-70">
                         <span x-show="!loading">Save changes</span>
-                        <span x-show="loading" class="inline-block animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-white mr-2"></span>
-                        <span x-show="loading">Saving...</span>
+                        <svg x-show="loading" class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" style="display: none;">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
                     </button>
                 </div>
             </div>
