e6f5a1b95c69f039daa28dd8e08db53a2cc62acf
Emils <emils.boreiko@gmail.com>
2026-03-09 14:00:29 +0200

    listings

--- resources/views/components/listings/we-got-your-back.blade.php
+++ resources/views/components/listings/we-got-your-back.blade.php
@@ -1,13 +1,7 @@
 <div class="bg-navy-blue rounded-[8px] pt-[58px] px-[24px] pb-[48px] text-white relative overflow-hidden flex flex-col justify-between h-[364px]">
     <!-- Background Graphic (Logo Mark) -->
-    <div class="absolute -bottom-[160px] -right-16 w-80 h-80 opacity-[0.07] pointer-events-none">
-        <svg viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full h-full rotate-[-10deg]">
-            <path d="M17.489 22.2789L17.9823 27.2133C16.6858 27.615 15.3082 27.8312 13.8801 27.8312C12.452 27.8312 11.0744 27.615 9.77783 27.2133L10.2713 22.2789H17.489Z" fill="white"/>
-            <path d="M5.00179 16.6149L4.43089 22.2792H2.7855C1.0367 19.9509 0 17.0548 0 13.9158C0 8.978 2.56522 4.64096 6.43244 2.17095L5.00179 16.6149Z" fill="white"/>
-            <path d="M10.271 22.2791H6.38379L6.93492 16.8107L6.93497 16.8099L8.49231 1.08723C9.74063 0.559643 11.082 0.209839 12.4849 0.0694805L12.4919 3.8147e-05L10.271 22.2791Z" fill="white"/>
-            <path d="M22.7582 16.6149L23.3291 22.2792H24.9745C26.7234 19.9509 27.7599 17.0548 27.7599 13.9158C27.7599 8.978 25.1947 4.64096 21.3276 2.17095L22.7582 16.6149Z" fill="white"/>
-            <path d="M17.4889 22.2791H21.3762L20.825 16.8107L20.8249 16.8099L19.2676 1.08723C18.0192 0.559643 16.6779 0.209839 15.275 0.0694805L15.2681 3.8147e-05L17.4889 22.2791Z" fill="white"/>
-        </svg>
+    <div class="w-[266px] -bottom-[130px] -right-[43px] absolute opacity-5 pointer-events-none">
+        <img src="{{ asset('images/hopinhome_symbol_white.svg') }}" alt="" class="w-full h-auto" style="max-height: 80vh;">
     </div>
 
     <div class="relative z-10">
@@ -18,7 +12,7 @@
     </div>
 
     <div class="relative z-10">
-        <button class="h-[52px] border border-white rounded-full px-8 py-2.5 text-[16px] font-medium hover:bg-white hover:text-navy-blue transition-colors duration-200">
+        <button class="h-[52px] bg-navy-blue border border-white rounded-full px-8 py-2.5 text-[16px] font-medium hover:bg-white hover:text-navy-blue transition-colors duration-200">
             Learn more
         </button>
     </div>
