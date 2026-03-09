33b1af0420c89e2cfb2eacbef0d71ed948cd3455
Emils <emils.boreiko@gmail.com>
2026-03-09 10:57:18 +0200

    rentre

--- resources/views/renter/dashboard.blade.php
+++ resources/views/renter/dashboard.blade.php
@@ -10,7 +10,7 @@
             {{-- Footer Action --}}
             <div class="flex justify-center mt-[24px] mb-[128px]">
                 <button class="bg-white border border-[#e8e8e7] rounded-full flex items-center justify-center gap-2 h-[52px] w-[280px] hover:bg-gray-50 transition-colors">
-                    <img alt="arrow downward" class="w-[17px] h-[17px]" src="{{ asset('images/arrow_forward_gray.svg') }}">
+                    <img alt="arrow downward" class="w-[17px] h-[17px]" src="{{ asset('images/arrow_downward_gray.svg') }}">
                     <span class="font-medium text-[16px] text-[#1e1d1d] tracking-[-0.48px]">Show all reviews</span>
                 </button>
             </div>
