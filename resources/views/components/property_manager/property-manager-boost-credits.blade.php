8d462dc8e96e85a893314706d9313cd32d40131d
Emils <emils.boreiko@gmail.com>
2026-03-10 20:43:58 +0200

    tooltips

--- resources/views/components/property_manager/property-manager-boost-credits.blade.php
+++ resources/views/components/property_manager/property-manager-boost-credits.blade.php
@@ -8,7 +8,35 @@
                 </div>
                 <h3 class="text-[18px] font-medium text-white">Boost credits</h3>
             </div>
-            <img src="{{ asset('images/info_white.svg') }}" alt="Info" class="w-5 h-5">
+            <div x-data="{ open: false }" class="relative">
+                <img 
+                    src="{{ asset('images/info_white.svg') }}" 
+                    alt="Info" 
+                    class="w-5 h-5 cursor-pointer"
+                    @mouseenter="open = true"
+                    @mouseleave="open = false"
+                >
+                <div 
+                    x-show="open"
+                    x-cloak
+                    x-transition:enter="transition ease-out duration-200"
+                    x-transition:enter-start="opacity-0 translate-y-1"
+                    x-transition:enter-end="opacity-100 translate-y-0"
+                    x-transition:leave="transition ease-in duration-150"
+                    x-transition:leave-start="opacity-100 translate-y-0"
+                    x-transition:leave-end="opacity-0 translate-y-1"
+                    class="absolute z-50 bottom-full right-[-14px] mb-[18px] w-[441px] bg-white rounded-[6px] shadow-[0px_4px_16px_0px_rgba(0,0,0,0.15)] p-[14px] pointer-events-none"
+                >
+                    <p class="text-[16px] leading-[1.5] text-[#464646] mb-[18px] font-['General_Sans',_sans-serif]">
+                        Any unused boost credits included in your plan will expire with your next renewal on <span class="font-medium text-[#1e1d1d]">March 15, 2026.</span>
+                    </p>
+                    <p class="text-[16px] leading-[1.5] text-[#464646] font-['General_Sans',_sans-serif]">
+                        Additional boost credits that you purchased separately <span class="font-medium text-[#1e1d1d]">will roll over</span> to the next month and remain available as long as you have an active subscription.
+                    </p>
+                    {{-- Caret --}}
+                    <div class="absolute bottom-[-6px] right-[18px] w-3 h-3 bg-white rotate-45 border-r border-b border-gray-100"></div>
+                </div>
+            </div>
         </div>
 
         {{-- Values Section --}}
