57dd88d898c9c01dfe954f0db7e0b04c14e3f39d
Emils <emils.boreiko@gmail.com>
2026-03-12 16:43:01 +0200

    auth

--- resources/views/components/auth/step-layout.blade.php
+++ resources/views/components/auth/step-layout.blade.php
@@ -9,7 +9,7 @@
     <!-- Header -->
     <div class="px-[24px] py-[21px] border-b border-[#e8e8e7] flex items-center justify-between relative">
         @if($showBack)
-            <button @click="step = '{{ $backStep }}'; error = ''; passwordError = ''; otpError = ''; otpSuccessMessage = ''; clearInterval(resendInterval); resendTimer = 60; registerError = ''; verifyCode = ['', '', '', '', '', '']" 
+            <button @click="step = '{{ $backStep }}'; error = ''; passwordError = ''; otpError = ''; otpSuccessMessage = ''; clearInterval(resendInterval); resendTimer = 60; registerError = ''; verifyCode = ['', '', '', '', '', '']"
                     class="absolute left-[24px] text-[#1e1d1d] hover:opacity-70 transition-opacity z-10">
                 <img src="{{ asset('images/close_blue.svg') }}" class="w-6 h-6" alt="Back">
             </button>
@@ -18,8 +18,8 @@
                 <img src="{{ asset('images/close_blue.svg') }}" class="w-6 h-6" alt="Close">
             </button>
         @endif
-        
-        <h2 class="absolute inset-0 flex items-center justify-center text-[18px] font-medium text-[#1e1d1d] pointer-events-none">
+
+        <h2 class="w-full inset-0 flex items-center justify-center text-[18px] font-medium text-[#1e1d1d] pointer-events-none">
             {{ $title }}
         </h2>
     </div>
