26fe258afb3780da840c4727fba64754da1e8824
Emils <emils.boreiko@gmail.com>
2026-03-12 16:28:12 +0200

    auth

--- resources/views/components/otp-input.blade.php
+++ resources/views/components/otp-input.blade.php
@@ -1,44 +1,42 @@
 @props(['name' => 'otp-input', 'model' => 'verifyCode'])
 
-<div class="flex items-center justify-between p-[14px] border border-[#1447d4] rounded-[8px]" x-id="['otp-inputs']">
-    <template x-for="(code, index) in {{ $model }}" :key="index">
-        <div class="flex items-center">
-            <input type="text"
-                   maxlength="1"
-                   :data-index="index"
-                   class="{{ $name }} w-[51px] h-[51px] text-center text-[20px] font-medium border border-[#1447d4] rounded-[8px] focus:border-[#1447d4] focus:ring-1 focus:ring-[#1447d4] outline-none transition-colors"
-                   :class="{'bg-[#E8E8E7] border-[#E8E8E7]': {{ $model }}[index] !== ''}"
-                   x-model="{{ $model }}[index]"
-                   @input="
-                      otpError = '';
-                      if ($event.target.value.length === 1 && index < 5) {
-                          $el.closest('.flex').nextElementSibling?.querySelector('input')?.focus();
+<div class="flex items-center justify-between p-[14px] border border-[#E8E8E7] rounded-[8px]" x-id="['otp-inputs']">
+    @for ($i = 0; $i < 6; $i++)
+        @if ($i === 3)
+            <div class="w-[14px] h-[2px] bg-gray-400 rounded-full"></div>
+        @endif
+        <input type="text"
+               maxlength="1"
+               data-index="{{ $i }}"
+               class="{{ $name }} w-[51px] h-[51px] text-center text-[20px] font-medium border border-[#1447d4] rounded-[8px] focus:border-[#1447d4] focus:ring-1 focus:ring-[#1447d4] outline-none transition-colors"
+               :class="{'bg-[#E8E8E7] border-[#E8E8E7]': {{ $model }}[{{ $i }}] !== ''}"
+               x-model="{{ $model }}[{{ $i }}]"
+               @input="
+                  otpError = '';
+                  if ($event.target.value.length === 1 && {{ $i }} < 5) {
+                      $el.parentElement.querySelectorAll('.{{ $name }}')[{{ $i + 1 }}]?.focus();
+                  }
+               "
+               @keydown.backspace="
+                  if ($event.target.value.length === 0 && {{ $i }} > 0) {
+                      $el.parentElement.querySelectorAll('.{{ $name }}')[{{ $i - 1 }}]?.focus();
+                  }
+               "
+               @paste.prevent="
+                  otpError = '';
+                  let paste = ($event.clipboardData || window.clipboardData).getData('text');
+                  paste = paste.replace(/\D/g, '').substring(0, 6);
+                  for (let i = 0; i < paste.length; i++) {
+                      if ({{ $i }} + i < 6) {
+                          {{ $model }}[{{ $i }} + i] = paste[i];
                       }
-                   "
-                   @keydown.backspace="
-                      if ($event.target.value.length === 0 && index > 0) {
-                          $el.closest('.flex').previousElementSibling?.querySelector('input')?.focus();
-                      }
-                   "
-                   @paste.prevent="
-                      otpError = '';
-                      let paste = ($event.clipboardData || window.clipboardData).getData('text');
-                      paste = paste.replace(/\D/g, '').substring(0, 6);
-                      for (let i = 0; i < paste.length; i++) {
-                          if (index + i < 6) {
-                              {{ $model }}[index + i] = paste[i];
-                          }
-                      }
-                      $nextTick(() => {
-                          let inputs = $el.closest('.flex').parentElement.querySelectorAll('.{{ $name }}');
-                          let focusIndex = Math.min(index + paste.length, 5);
-                          if (inputs[focusIndex]) inputs[focusIndex].focus();
-                      });
-                   "
-            >
-            <div x-show="index === 2" class="flex items-center justify-center w-[14px] mx-1">
-                <div class="w-full h-[2px] bg-gray-400 rounded-full"></div>
-            </div>
-        </div>
-    </template>
+                  }
+                  $nextTick(() => {
+                      let inputs = $el.parentElement.querySelectorAll('.{{ $name }}');
+                      let focusIndex = Math.min({{ $i }} + paste.length, 5);
+                      if (inputs[focusIndex]) inputs[focusIndex].focus();
+                  });
+               "
+        >
+    @endfor
 </div>
