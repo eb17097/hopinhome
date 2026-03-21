4b4687b414889dddeb42f18d21a45ba5cd037b3d
Emils <emils.boreiko@gmail.com>
2026-03-21 17:57:06 +0200

    onboarding

--- resources/views/components/onboarding/layout.blade.php
+++ resources/views/components/onboarding/layout.blade.php
@@ -44,7 +44,7 @@
 
         <!-- Right Side (Blue) -->
         <div class="hidden lg:block w-1/2 bg-[#1447d4] relative overflow-hidden">
-            <div style="bottom:-20%; right:-25%; width: 120%;" class="absolute opacity-10">
+            <div style="bottom:-25%; right:-25%; width: 120%;" class="absolute opacity-10">
                 <img src="{{ asset('images/hopinhome_symbol_white.svg') }}" alt="" class="w-full h-auto" style="max-height: 80vh;">
             </div>
         </div>
